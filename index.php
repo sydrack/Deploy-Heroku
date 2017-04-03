6ff03f04c99b07f1db070a6cbda9d9d457791992
sydrack <sydrack@edu.univali.br>
2017-04-03 11:43:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 define("DATABASE", "host=ec2-54-83-49-44.compute-1.amazonaws.com port=5432 dbname=dekono9ngjih35 user=sedjlmulcvnfpf password=fa2bf0358fa9ac7eb9370076fc15023dd058ce6aceade8427dafdf890ea4f893");
 
-echo "Ola";
  
 $db = pg_connect (DATABASE);
 $consulta = pg_query($db, "SELECT * FROM usuario");
